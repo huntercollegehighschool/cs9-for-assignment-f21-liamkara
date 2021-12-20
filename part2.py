9120aa9ffd6a6b2fdc647886fa48accf1a4b3736
Liam Karamanoglou <93930220+liamkara@users.noreply.github.com>
2021-12-20 15:10:47 +0000

    Part 1 code fixed

--- part2.py
+++ part2.py
@@ -8,3 +8,11 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+n = int(input("Enter a number: "))
+total = 0
+
+while n != 0:
+  total = total + n * n * n
+  n = n -1
+
+print(total)
